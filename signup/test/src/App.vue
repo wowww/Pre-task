b29b2abc9495829d38dc183fd6b103e786929a16
Lia, Eunjee <58850132+wowww@users.noreply.github.com>
2020-11-21 07:11:29 +0900

    feat. 회원가입 UI

---
+++ signup/test/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <NavBar />
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/signup">SignUp</router-link>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import NavBar from "./components/NavBar"
+  export default {
+    components: {
+      NavBar
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ signup/test/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+import Login from '@/pages/login'
+import SignUp from '@/pages/signup'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: SignUp
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
